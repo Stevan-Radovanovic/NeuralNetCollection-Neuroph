1abac53c54e9d10a9583c435194633c1e22f9993
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-06-16 20:18:37 +0200

    WinesRedux Commit

--- NeurophWines/src/neurophwines/NeurophWines.java
+++ NeurophWines/src/neurophwines/NeurophWines.java
@@ -16,7 +16,8 @@ public class NeurophWines {
 
     ArrayList<Training> trainings = new ArrayList<Training>();
     double[] learningRates = {0.2,0.4,0.6};
-    int[] hiddenNeurons = {20};
+    String[] labels = new String[]{"s1","s2","s3"};
+    int hiddenNeurons = 20;
     int input=13;
     int output=3;
     
@@ -26,61 +27,44 @@ public class NeurophWines {
     
     public void run () {
         
-        System.out.println("Reading dataset from csv...");
-        DataSet data = DataSet.createFromFile("wines.csv",input,output,",");
-        MaxNormalizer norm = new MaxNormalizer(data);
-        System.out.println("Normalizing data...");
-        norm.normalize(data);
-        System.out.println("Shuffling data...");
+        System.out.println("Creating. Normalizing. Shuffling. Splitting");
+        DataSet data = DataSet.createFromFile("wines.csv", input, output, ",");
+        MaxNormalizer max = new MaxNormalizer(data);
+        max.normalize(data);
         data.shuffle();
-        
-        int numOfTrainings=0, numOfIterations=0;
-        
-        System.out.println("Spliting data...");
-        DataSet[] yo = data.split(0.7,0.3);
+        DataSet yo[] = data.split(0.65,0.35);
         DataSet train = yo[0];
         DataSet test = yo[1];
         
+        int numberOfIterations=0;
+        int numberOfTrainings = 0;
+        
         for(double lr: learningRates) {
-            for(int hn: hiddenNeurons) {
-                MultiLayerPerceptron neuralNet = new MultiLayerPerceptron(input,hn,output);
-                neuralNet.setLearningRule(new MomentumBackpropagation());
-                MomentumBackpropagation mbp = (MomentumBackpropagation) neuralNet.getLearningRule();
-                mbp.setLearningRate(lr);
-                mbp.setMaxError(0.02);
-                mbp.setMomentum(0.6);
-                
-                neuralNet.learn(train);
-                
-                numOfTrainings++;
-                numOfIterations+=mbp.getCurrentIteration();
-                
-                System.out.println("Commencing Training no." + numOfTrainings);
-                
-                double accuracy = 0;
-                double msne = 0;
-
-                
-                if(output==1) {
-                   accuracy = calculateAccuracyOneOutput(neuralNet, test);
-                   msne = calculateMsneOneOutput(neuralNet, test);
-                } else {
-                   accuracy =  calculateAccuracyMultiOutput(neuralNet, test);
-                   msne = calculateMsneMultiOutput(neuralNet, test);
-                }
-              
-                trainings.add(new Training(neuralNet,msne,accuracy));
-            }
+            MultiLayerPerceptron neuralNet = new MultiLayerPerceptron(input,hiddenNeurons,output);
+            neuralNet.setLearningRule(new MomentumBackpropagation());
+            MomentumBackpropagation mbp = (MomentumBackpropagation) neuralNet.getLearningRule();
+            
+            mbp.setMomentum(0.6);
+            mbp.setLearningRate(lr);
+            mbp.setMaxError(0.007);
+            mbp.setMaxIterations(500);
+            
+            neuralNet.learn(train);
+            
+            double accuracy = calculateAccuracy(neuralNet,test);
+            double msne = calculateMsne(neuralNet,test);
+            
+            numberOfIterations+=mbp.getCurrentIteration();
+            numberOfTrainings++;
+            
+            trainings.add(new Training(neuralNet,msne,accuracy));
         }
         
+        System.out.println("Average iterations: " + (double)numberOfIterations/numberOfTrainings);
         serializeMaxAcc();
-        System.out.println("Iterations average: " + (double)numOfIterations/numOfTrainings);
         
     }
-   
-    
-    
-    
+
     public void serializeMaxAcc() {
         Training max = trainings.get(0);
         for(Training t : trainings) {
@@ -89,155 +73,67 @@ public class NeurophWines {
             }
         }
         System.out.println("Serializing...");
-        max.neuralNetwork.save("maxacc.nnet");
+        //max.neuralNetwork.save("maxacc.nnet");
     }
-    
-    public double calculateAccuracyOneOutput(MultiLayerPerceptron neuralNet, DataSet test) {
-       
-        int tp=0,tn=0,fp=0,fn=0;
+
+    private double calculateAccuracy(MultiLayerPerceptron neuralNet, DataSet test) {
+           
+        ConfusionMatrix cm = new ConfusionMatrix(labels);
+        double sum = 0;
         
-        for(DataSetRow row: test) {
-            neuralNet.setInput(row.getInput());
-            neuralNet.calculate();
-            
-            double actual = row.getDesiredOutput()[0];
-            double predicted = neuralNet.getOutput()[0];
-            
-            if(actual==1 && predicted>0.5) tp++;
-            if(actual==0 && predicted<=0.5) tn++;
-            if(actual==1 && predicted<=0.5) fn++;
-            if(actual==0 && predicted>0.5)  fp++;
-        }
-        
-        double accuracy = (double) (tp+tn)/(tp+tn+fp+fn);
-        System.out.println("Accuracy: " + accuracy);
-        return accuracy;
-        
-    }
-    
-    public double calculateAccuracyMultiOutput(MultiLayerPerceptron neuralNet, DataSet test) {
-        int tp=0,tn=0,fp=0,fn=0;
-        
-        String[] classLabels = {"s1","s2","s3"};
-        ConfusionMatrix cm = new ConfusionMatrix(classLabels);
-        
-        for(DataSetRow row: test) {
+        for(DataSetRow row : test) {
             neuralNet.setInput(row.getInput());
             neuralNet.calculate();
             
             int actual = getMaxIndexArray(row.getDesiredOutput());
             int predicted = getMaxIndexArray(neuralNet.getOutput());
-           
+            
             cm.incrementElement(actual, predicted);
         }
         
-        System.out.println(cm.toString());
-        
-        double accuracy=0;
-        
         for(int i=0;i<output;i++) {
-            tp = cm.getTruePositive(i);
-            tn = cm.getTrueNegative(i);
-            fp = cm.getFalsePositive(i);
-            fn = cm.getFalseNegative(i);
-            
-            accuracy += (double)(tp+tn)/(tp+tn+fp+fn);
+            int tp = cm.getTruePositive(i);
+            int tn = cm.getTrueNegative(i);
+            int fp = cm.getFalsePositive(i);
+            int fn = cm.getFalseNegative(i);
+            sum += (double)(tp+tn)/(tp+tn+fn+fp);
         }
         
-        System.out.println("Accuracy: " + accuracy/output);
-        return (double) accuracy/output;
-    }
-    
-    public double calculateMsneOneOutput(MultiLayerPerceptron neuralNet, DataSet test) {
-        
-       double sumError=0,msne=0;
-       
-       for(DataSetRow row : test) {
-           neuralNet.setInput(row.getInput());
-           neuralNet.calculate();
-           
-           double actual = row.getDesiredOutput()[0];
-           double predicted = neuralNet.getOutput()[0];
-           
-           sumError+=Math.pow(predicted-actual, 2);
-       }
-       
-       msne = (double)sumError/(2*test.size());
-       System.out.println("MSNE: " + msne);
-       return msne; 
-    }
-    
-    public double calculateMsneMultiOutput(MultiLayerPerceptron neuralNet, DataSet test) {
-               
-       double sumError=0,msne=0;
-       
-       for(DataSetRow row : test) {
-           neuralNet.setInput(row.getInput());
-           neuralNet.calculate();
-           
-           double[] actual = row.getDesiredOutput();
-           double[] predicted = neuralNet.getOutput();
-           
-           for(int i=0;i<actual.length;i++) {
-            sumError+=Math.pow(predicted[i]-actual[i], 2);
-           }
-       }
-       
-       msne = (double)sumError/(2*test.size());
-       System.out.println("MSNE: " + msne);
-       return msne;
-    }
-    
-    public void calculateAccuracyNeurophOneOutput(MultiLayerPerceptron neuralNet, DataSet test) {
-        Evaluation evaluation = new Evaluation();
-        evaluation.addEvaluator(new ClassifierEvaluator.Binary(0.5));
-        evaluation.evaluate(neuralNet, test);
-        
-        ConfusionMatrix cm = evaluation.getEvaluator(ClassifierEvaluator.Binary.class).getResult();
-        ClassificationMetrics[] metrics = ClassificationMetrics.createFromMatrix(cm);
-        ClassificationMetrics.Stats average = ClassificationMetrics.average(metrics);
-        
-        System.out.println("Accuracy Neuroph: " + average.accuracy);
-    }
-    
-    public void calculateAccuracyNeurophMultiOutput(MultiLayerPerceptron neuralNet, DataSet test) {
-        Evaluation evaluation = new Evaluation();
-        evaluation.addEvaluator(new ClassifierEvaluator.MultiClass(new String[]{"s1","s2","s3"}));
-        evaluation.evaluate(neuralNet, test);
-        
-        ConfusionMatrix cm = evaluation.getEvaluator(ClassifierEvaluator.MultiClass.class).getResult();
-        ClassificationMetrics[] metrics = ClassificationMetrics.createFromMatrix(cm);
-        ClassificationMetrics.Stats average = ClassificationMetrics.average(metrics);
-        
-        System.out.println("Accuracy Neuroph: " + average.accuracy);
-    }
-    
-    public void calculateMsneNeurophOneOutput(MultiLayerPerceptron neuralNet, DataSet test) {
-        Evaluation evaluation = new Evaluation();
-        evaluation.addEvaluator(new ClassifierEvaluator.Binary(0.5));
-        evaluation.evaluate(neuralNet, test);
-        
-        System.out.println("MSNE: " + evaluation.getMeanSquareError());
-    }
-    
-    public void calculateMsneNeurophMultiOutput(MultiLayerPerceptron neuralNet, DataSet test) {
-        Evaluation evaluation = new Evaluation();
-        evaluation.addEvaluator(new ClassifierEvaluator.MultiClass(new String[]{"s1","s2","s3"}));
-        evaluation.evaluate(neuralNet, test);
-        
-        System.out.println("MSNE: " + evaluation.getMeanSquareError());
+        double acc = (double)sum/output;
+        System.out.println("Accuracy: " + acc);
+        return acc;
         
     }
-    
-    public int getMaxIndexArray(double[] array) {
-        int max = 0;
-        for(int i=0;i<array.length;i++) {
-            if(array[max]<array[i]) {
-                max=i;
+
+    private double calculateMsne(MultiLayerPerceptron neuralNet, DataSet test) {
+        double sum = 0;
+        
+        for(DataSetRow row : test) {
+            neuralNet.setInput(row.getInput());
+            neuralNet.calculate();
+            
+            double[] actual = row.getDesiredOutput();
+            double[] predicted = neuralNet.getOutput();
+            
+            for(int i=0;i<actual.length;i++) {
+                sum+=Math.pow(actual[i]-predicted[i],2);
             }
         }
         
-        return max;
+             double msne = (double)sum/(2*test.size());
+             System.out.println("Msne: " + msne);
+             return msne;
+    }
+
+    private int getMaxIndexArray(double[] array) {
+
+        int position = 0;
+        for(int i=0;i<array.length;i++) {
+            if(array[i]>array[position]) position=i;
+        }
+        
+        return position;
+        
     }
     
 }
